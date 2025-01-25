a1f3ffc72b88d2cdbf82f3e3cd2b2fccd845bb7e
Seven SUN <i@sunyz.net>
2025-01-25 20:26:24 +0800

    [+] add terminal output colors
    Signed-off-by: Seven SUN <i@sunyz.net>

--- src/go.mod
+++ src/go.mod
@@ -1,3 +1,3 @@
-module github.com/realsunyz/geofeed-validator
+module github.com/realsunyz/geofeed-tools
 
-go 1.23.2
+go 1.23
---
+++ src/plugin/color/main.go
@@ -0,0 +1,10 @@
+package color
+
+const (
+	Reset   = "\033[0m"
+	Red     = "\033[31m"
+	Green   = "\033[32m"
+	Yellow  = "\033[33m"
+	Magenta = "\033[35m"
+	Cyan    = "\033[36m"
+)
